81a9b9b28fb978854ca12dbc9234cc106108e6a5
timotheyca <timotheyca@users.noreply.github.com>
2018-12-07 20:10:58 +0300

    Update BdBeta.cs

--- build/src/Stuff/BdBeta.cs
+++ build/src/Stuff/BdBeta.cs
@@ -47,13 +47,7 @@ namespace TMGmod.Stuff
             for (var i = 0; i < 8; ++i)
             {
                 var barricade = new BarricadeBeta(vec2.x, vec2.y - i * 4);
-                if (Network.isActive && isServerForObject)
-                {
-                    if (duck?.profile.connection != null)
-                    {
-                        barricade.connection = duck.profile.connection;
-                    }
-                }
+                Fondle(barricade);
                 Level.Add(barricade);
             }
         }
