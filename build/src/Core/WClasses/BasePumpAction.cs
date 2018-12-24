87bc4617a8ba78d2e19b8069e4d1a4500038d1be
timotheyca <timotheyca@users.noreply.github.com>
2018-12-24 21:57:30 +0300

    Update BasePumpAction.cs

--- build/src/Core/WClasses/BasePumpAction.cs
+++ build/src/Core/WClasses/BasePumpAction.cs
@@ -3,28 +3,28 @@ using DuckGame;
 
 namespace TMGmod.Core.WClasses
 {
-    public class BasePumpAction:BaseGun, IAmSg
+    public abstract class BasePumpAction:BaseGun, IAmSg
     {
-        public sbyte _loadProgress = 100;
-        public float _loadAnimation = 1f;
-        public StateBinding _loadProgressBinding = new StateBinding(nameof(_loadProgress), -1, false, false);
-        protected SpriteMap _loaderSprite;
+        private sbyte _loadProgress = 100;
+        private float _loadAnimation = 1f;
+        public StateBinding LoadProgressBinding = new StateBinding(nameof(_loadProgress), -1, false, false);
+        private SpriteMap _loaderSprite;
 
-        public BasePumpAction(float xval, float yval) : base(xval, yval)
+        protected BasePumpAction(float xval, float yval) : base(xval, yval)
         {
         }
 
         public override void Update()
         {
             base.Update();
-            if (_loadAnimation == -1.0)
+            if (Math.Abs(_loadAnimation - (-1.0)) < 0.02f)
             {
-                SFX.Play("shotgunLoad", 1f, 0.0f, 0.0f, false);
+                SFX.Play("shotgunLoad");
                 _loadAnimation = 0.0f;
             }
             if (_loadAnimation >= 0.0)
             {
-                if (_loadAnimation == 0.5 && ammo != 0)
+                if (Math.Abs(_loadAnimation - 0.5) < 0.02f && ammo != 0)
                     _ammoType.PopShell(x, y, -offDir);
                 if (_loadAnimation < 1.0)
                     _loadAnimation += 0.1f;
@@ -61,9 +61,9 @@ namespace TMGmod.Core.WClasses
         public override void Draw()
         {
             base.Draw();
-            Vec2 vec2 = new Vec2(13f, -2f);
-            float num = (float)Math.Sin(_loadAnimation * 3.14000010490417) * 3f;
-            Draw(_loaderSprite, new Vec2(vec2.x - 8f - num, vec2.y + 4f), 1);
+            var vec2 = new Vec2(13f, -2f);
+            var num = (float)Math.Sin(_loadAnimation * 3.14000010490417) * 3f;
+            Draw(_loaderSprite, new Vec2(vec2.x - 8f - num, vec2.y + 4f));
         }
     }
 }
