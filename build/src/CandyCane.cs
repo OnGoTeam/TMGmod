9888841eee6e41cf4f9a8a5d7ec9361989db3d47
timotheyca <timotheyca@users.noreply.github.com>
2018-12-29 17:02:47 +0300

    NY0.2

--- build/src/CandyCane.cs
+++ build/src/CandyCane.cs
@@ -1,41 +1,40 @@
 ﻿using DuckGame;
-// ReSharper disable VirtualMemberCallInConstructor
+using TMGmod.Core.AmmoTypes;
 
 namespace TMGmod
 {
+    [EditorGroup("TMG|Misc")]
     public class CandyCane:Gun
     {
         public CandyCane(float xval, float yval) : base(xval, yval)
         {
             ammo = 1;
-            _ammoType = new ATMagnum
+            _ammoType = new ATCandyCane
             {
-                range = 150f,
-                accuracy = 0.95f,
-                penetration = 1f
+                sprite = new Sprite(GetPath("Holiday/candycane"))
             };
-            _numBulletsPerFire = 2;
             _type = "gun";
-            graphic = new Sprite(GetPath("candycane"));
-            center = new Vec2(9f, 3.5f);
-            collisionOffset = new Vec2(-9f, -3.5f);
-            collisionSize = new Vec2(18f, 7f);
+            _graphic = new Sprite(GetPath("Holiday/candycane"));
+            _center = new Vec2(9f, 3.5f);
+            _collisionOffset = new Vec2(-9f, -3.5f);
+            _collisionSize = new Vec2(18f, 7f);
             _barrelOffsetTL = new Vec2(18f, 3.5f);
             _fireSound = "pistolFire";
             _fullAuto = false;
-            _manualLoad = true;
             _fireWait = 1.2f;
             _kickForce = 0f;
             loseAccuracy = 0f;
             maxAccuracyLost = 0f;
             _holdOffset = new Vec2(-1f, 1f);
-            _editorName = "AF-2011";
-            weight = 2.5f;
-            
+            _editorName = "woodHit";
+            _weight = 2.5f;
         }
 
         public override void Reload(bool shell = true)
         {
+            if (loaded) return;
+            duck?.ThrowItem(false);
+            Level.Remove(this);
         }
     }
 }
---
+++ build/src/Core/AmmoTypes/ATCandyCane.cs
@@ -0,0 +1,19 @@
+﻿using DuckGame;
+using TMGmod.Core.Bullets;
+
+namespace TMGmod.Core.AmmoTypes
+{
+    // ReSharper disable once InconsistentNaming
+    public class ATCandyCane : AmmoType
+    {
+        public ATCandyCane()
+        {
+            bulletType = typeof(CandyCaneBullet);
+            bulletSpeed = 15f;
+            range = 500f;
+            accuracy = 0.95f;
+            bulletLength = 3f;
+            //sprite = new Sprite(GetPath("Holiday/candycane"));
+        }
+    }
+}
