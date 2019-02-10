9b2542a99ebbf8bb4a7be751dcf5ede60d0bbdc7
timotheyca <timotheyca@users.noreply.github.com>
2019-02-10 16:53:11 +0300

    SFE

---
+++ build/src/SuperFE.cs
@@ -0,0 +1,25 @@
+﻿using DuckGame;
+using JetBrains.Annotations;
+
+namespace TMGmod
+{
+    [EditorGroup("TMG|Misc")]
+    [PublicAPI]
+    public class SuperFe:FireExtinguisher
+    {
+        public SuperFe(float xval, float yval) : base(xval, yval)
+        {
+            _kickForce = 12f;
+            _editorName = "SFE";
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (_firing && ammo > 0)
+            {
+                ApplyKick();
+            }
+        }
+    }
+}
