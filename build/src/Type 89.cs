9936885f97d5ed3c5c084e0af86736bbdc156840
EXsess <45515382+EXsess@users.noreply.github.com>
2019-03-26 02:49:42 +0300

    New
    +пушка и небольшые change текстурок

--- build/src/Type 89.cs
+++ build/src/Type 89.cs
@@ -3,35 +3,35 @@ using TMGmod.Core.WClasses;
 
 namespace TMGmod.Custom_Guns
 {
-    [EditorGroup("TMG|Machinegun|Custom")]
-    public class M4A1C : BaseAr
+    [EditorGroup("TMG|Machinegun")]
+    public class Type89 : BaseAr
     {
 		
-		public M4A1C (float xval, float yval)
+		public Type89(float xval, float yval)
           : base(xval, yval)
         {
-            ammo = 30;
+            ammo = 26;
             _ammoType = new AT9mm
             {
-                range = 300f,
+                range = 265f,
                 accuracy = 0.91f,
                 penetration = 1.5f
             };
             _type = "gun";
-            _graphic = new Sprite(GetPath("m4a1Custm"));
+            _graphic = new Sprite(GetPath("Type 89"));
             _center = new Vec2(15f, 6f);
             _collisionOffset = new Vec2(-15f, -6f);
             _collisionSize = new Vec2(30f, 12f);
             _barrelOffsetTL = new Vec2(31f, 5f);
             _fireSound = "deepMachineGun";
             _fullAuto = true;
-            _fireWait = 0.65f;
-            _kickForce = 0f;
-            loseAccuracy = 0.01f;
-            maxAccuracyLost = 0.09f;
-            _holdOffset = new Vec2(2f, 0f);
-            _editorName = "M4A1 Custom";
-			_weight = 4f;
+            _fireWait = 0.71f;
+            _kickForce = 1.2f;
+            loseAccuracy = 0.05f;
+            maxAccuracyLost = 0.4f;
+            _holdOffset = new Vec2(1f, 0f);
+            _editorName = "Type 89";
+			_weight = 4.6f;
         }
     }
 }
