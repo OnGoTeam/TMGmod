cec1f81577d8f694bc729a114f22aff98d8763c9
EXsess <45515382+EXsess@users.noreply.github.com>
2019-04-28 15:59:50 +0300

    ы

--- build/Properties/AssemblyInfo.cs
+++ build/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
 // общие сведения об этой сборке.
 [assembly: AssemblyTitle("TMGmod")]
-[assembly: AssemblyDescription("Current update: Skins in pills")]
+[assembly: AssemblyDescription("Current update: MAPS 2.0")]
 //[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OnGoTeam")]
 [assembly: AssemblyProduct("TMGmod")]
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 
 #if WORKSHOP
-[assembly: AssemblyVersion("1.1.1.3")]
+[assembly: AssemblyVersion("1.1.1.4")]
 #else
 [assembly: AssemblyVersion("1.101.*")]
 #endif
