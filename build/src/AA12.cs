123bb541dbd0f277b9d405932ea57eb6fafddefb
Exel Shustrov <45515382+EXsess@users.noreply.github.com>
2019-07-11 21:53:43 +0300

    AA-12+

--- build/src/AA12.cs
+++ build/src/AA12.cs
@@ -20,7 +20,7 @@ namespace TMGmod
         /// <inheritdoc />
         // ReSharper disable once ConvertToAutoProperty
         public EditorProperty<int> Skin => skin;
-        private static readonly List<int> Allowedlst = new List<int>(new[] { 0, 5 });
+        private static readonly List<int> Allowedlst = new List<int>(new[] { 0, 5, 7, 8 });
 
         public AA12 (float xval, float yval)
             : base(xval, yval)
