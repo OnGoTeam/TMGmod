6ace4a040e49b62b8a5ba513ac47de79d2381be7
Exel Shustrov <45515382+EXsess@users.noreply.github.com>
2019-07-12 19:10:16 +0300

    MP40 changeddd

--- build/src/MP40.cs
+++ build/src/MP40.cs
@@ -1,4 +1,7 @@
-﻿using DuckGame;
+﻿using System.Collections.Generic;
+using DuckGame;
+using JetBrains.Annotations;
+using TMGmod.Core;
 using TMGmod.Core.WClasses;
 
 namespace TMGmod
@@ -6,11 +9,22 @@ namespace TMGmod
 
     [BaggedProperty("isInDemo", true), EditorGroup("TMG|SMG|Fully-Automatic")]
     // ReSharper disable once InconsistentNaming
-    public class MP40 : BaseSmg, IFirstPrecise
+    public class MP40 : BaseSmg, IFirstPrecise, IHaveSkin
     {
+        private readonly SpriteMap _sprite;
+        private const int NonSkinFrames = 1;
+        public StateBinding FrameIdBinding { get; } = new StateBinding(nameof(FrameId));
+        [UsedImplicitly]
+        // ReSharper disable once InconsistentNaming
+        private readonly EditorProperty<int> skin;
+        /// <inheritdoc />
+        // ReSharper disable once ConvertToAutoProperty
+        public EditorProperty<int> Skin => skin;
+        private static readonly List<int> Allowedlst = new List<int>(new[] { 0, 7, 8 });
         public MP40(float xval, float yval)
             : base(xval, yval)
         {
+            skin = new EditorProperty<int>(0, this, -1f, 9f, 0.5f);
             ammo = 32;
             _ammoType = new AT9mm
             {
@@ -20,27 +34,46 @@ namespace TMGmod
             };
             BaseAccuracy = 0.5f;
             _type = "gun";
-            _graphic = new Sprite(GetPath("MP40"));
-            _center = new Vec2(12f, 5f);
-            _collisionOffset = new Vec2(-11f, -4f);
-            _collisionSize = new Vec2(22f, 14f);
-            _barrelOffsetTL = new Vec2(25f, 2f);
+            _sprite = new SpriteMap(GetPath("MP40"), 23, 14);
+            _graphic = _sprite;
+            _sprite.frame = 0;
+            _center = new Vec2(12f, 7f);
+            _collisionOffset = new Vec2(-12f, -7f);
+            _collisionSize = new Vec2(23f, 14f);
+            _barrelOffsetTL = new Vec2(23f, 2f);
             _fireSound = "deepMachineGun";
             _fullAuto = true;
             _fireWait = 0.5f;
             _kickForce = 0.5f;
-            _holdOffset = new Vec2(3f, 1f);
-            //loseAccuracy = 0.1f;
-            //maxAccuracyLost = 0.5f;
+            _holdOffset = new Vec2(4f, 4f);
             _editorName = "MP40";
 			_weight = 3f;
             KforceDSmg = 2.5f;
-            MaxDelaySmg = 50;
+            MaxDelaySmg = 20;
             MaxAccuracy = 1f;
             MaxDelayFp = 10;
+            loseAccuracy = 0.1f;
+            maxAccuracyLost = 0.4f;
         }
-
-        
+        private void UpdateSkin()
+        {
+            var bublic = Skin.value;
+            while (!Allowedlst.Contains(bublic))
+            {
+                bublic = Rando.Int(0, 9);
+            }
+            _sprite.frame = bublic;
+        }
+        public int FrameId
+        {
+            get => _sprite.frame;
+            set => _sprite.frame = value % (10 * NonSkinFrames);
+        }
+        public override void EditorPropertyChanged(object property)
+        {
+            UpdateSkin();
+            base.EditorPropertyChanged(property);
+        }
         public int CurrDelay { get; set; }
         public int MaxDelayFp { get; }
         public float MaxAccuracy { get; }
