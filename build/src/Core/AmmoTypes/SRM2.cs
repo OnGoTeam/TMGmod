bbe00b33d0756f6aea577027e35ec7e032e72179
timotheyca <12258572+timotheyca@users.noreply.github.com>
2019-08-05 17:16:30 +0300

    -srmdocs

--- build/src/Core/AmmoTypes/SRM2.cs
+++ build/src/Core/AmmoTypes/SRM2.cs
@@ -3,14 +3,8 @@ using DuckGame;
 namespace TMGmod.Core.AmmoTypes
 {
     // ReSharper disable once InconsistentNaming
-    /// <summary>
-    /// <see cref="AmmoType"/> with invisible bullets for silenced weapons
-    /// </summary>
     public class ATSRM2 : AmmoType
     {
-        /// <summary>
-        /// sets bulletLength to 0f for invisibility
-        /// </summary>
         public ATSRM2()
         {
             range = 120f;
