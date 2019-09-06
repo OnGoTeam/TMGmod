5da7c5d0be8c00dcbe619c7a6a01193204720d05
timotheyca <12258572+timotheyca@users.noreply.github.com>
2019-09-07 01:02:03 +0300

    BATT reworkt

--- build/src/Core/AmmoTypes/BaseAmmoTypeT.cs
+++ build/src/Core/AmmoTypes/BaseAmmoTypeT.cs
@@ -3,20 +3,8 @@ using JetBrains.Annotations;
 
 namespace TMGmod.Core.AmmoTypes
 {
-    /// <summary>
-    ///     AmmoType with GetPath
-    /// </summary>
     [PublicAPI]
     public abstract class BaseAmmoTypeT : AmmoType
     {
-        /// <summary>
-        ///     Implements GetPath for <see cref="AmmoType"/> through <see cref="Mod"/>.GetPath
-        /// </summary>
-        /// <param name="asset"></param>
-        /// <returns></returns>
-        protected string GetPath(string asset)
-        {
-            return Mod.GetPath<TMGmod>(asset);
-        }
     }
 }
