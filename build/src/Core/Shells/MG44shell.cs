4f50bd887ec7c4770ce4d63522bb8aa90e9cb2c2
timotheyca <12258572+timotheyca@users.noreply.github.com>
2019-11-27 23:38:20 +0300

    Update MG44shell.cs

--- build/src/Core/Shells/MG44shell.cs
+++ build/src/Core/Shells/MG44shell.cs
@@ -2,11 +2,17 @@ using DuckGame;
 
 namespace TMGmod.Core.Shells
 {
+    // ReSharper disable once InconsistentNaming
     public class MG44Shell : EjectedShell
     {
         public MG44Shell(float xpos, float ypos)
           : base(xpos, ypos, Mod.GetPath<TMGmod>("MG44Shell"))
         {
+        }
+        public override void Update()
+        {
+            base.Update();
+            _angle = Maths.DegToRad(-_spinAngle);
         }
     }
 }
