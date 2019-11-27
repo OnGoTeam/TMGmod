34aa14c6fb96378bf7b718970293a96f278a5fd5
timotheyca <12258572+timotheyca@users.noreply.github.com>
2019-11-27 23:41:57 +0300

    Update IHaveSkin.cs

--- build/src/Core/IHaveSkin.cs
+++ build/src/Core/IHaveSkin.cs
@@ -3,13 +3,17 @@ using JetBrains.Annotations;
 
 namespace TMGmod.Core
 {
-    public interface IHaveSkin
+    public interface IHaveSkin:IHaveFrameId
+    {
+        [UsedImplicitly]
+        EditorProperty<int> Skin { get; }
+    }
+
+    public interface IHaveFrameId
     {
         [UsedImplicitly]
         int FrameId { set; }
         [UsedImplicitly]
         StateBinding FrameIdBinding { get; }
-        [UsedImplicitly]
-        EditorProperty<int> Skin { get; }
     }
 }
