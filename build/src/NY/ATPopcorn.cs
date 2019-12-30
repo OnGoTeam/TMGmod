81242b6a1004bc1c044aa0396f468a9f456f229e
EXsess <extradan02@gmail.com>
2019-12-30 20:44:05 +0300

    Update ATPopcorn.cs

--- build/src/NY/ATPopcorn.cs
+++ build/src/NY/ATPopcorn.cs
@@ -15,17 +15,18 @@ namespace TMGmod.NY
             _sprite = new SpriteMap(Mod.GetPath<Core.TMGmod>("Holiday/Popcal"), 3, 3);
             _sprite.frame = Rando.Int(0, 8);
             sprite = _sprite;
-            bulletSpeed = 11f;
+            bulletSpeed = 8.5f;
             range = 400f;
             accuracy = 0.85f;
             bulletLength = 3f;
             penetration = 0.7f;
             affectedByGravity = true;
             weight = 0f;
-            barrelAngleDegrees = 0f;
+            barrelAngleDegrees = Rando.Float(11f, -11f);
         }
         public override Bullet FireBullet(Vec2 position, Thing owner = null, float angle = 0, Thing firedFrom = null)
         {
+            barrelAngleDegrees = Rando.Float(6f, -18f);
             _sprite.frame = Rando.Int(0, 8);
             sprite = _sprite;
             return base.FireBullet(position, owner, angle, firedFrom);
