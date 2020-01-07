59195fea8033d16d9b62273c909eeb76e6148d8e
EXsess <extradan02@gmail.com>
2020-01-07 21:43:46 +0300

    new proxi-gun

--- build/src/Remington.cs
+++ build/src/Remington.cs
@@ -6,47 +6,44 @@ namespace TMGmod
 {
     [UsedImplicitly]
     [EditorGroup("TMG|Shotgun|Pump-Action")]
-    public class Remington : BaseGun, IAmSg
+    public class Remington : BasePumpAction
     {
 	    public Remington(float xval, float yval) : base(xval, yval)
 	    {
-		    ammo = 11;
+		    ammo = 12;
 	        _ammoType = new AT9mm
 	        {
-	            range = 145f,
+	            range = 169f,
 	            accuracy = 0.47f,
 	            penetration = 1f,
-	            bulletSpeed = 16f,
+	            bulletSpeed = 50f,
 	            bulletThickness = 0.6f
 	        };
-            _numBulletsPerFire = 17;
+            _numBulletsPerFire = 16;
             _type = "gun";
-		    _graphic = new SpriteMap(GetPath("Taligator 6000 SX"), 31, 12);
-		    _center = new Vec2(16f, 6f);
-		    _collisionOffset = new Vec2(-16f, -6f);
-		    _collisionSize = new Vec2(31f, 12f);
-		    _barrelOffsetTL = new Vec2(31f, 3f);
-            _flare = new SpriteMap(GetPath("FlareOnePixel2"), 13, 10)
+		    _graphic = new SpriteMap(GetPath("Remington1"), 46, 10);
+		    _center = new Vec2(23f, 5f);
+		    _collisionOffset = new Vec2(-23f, -5f);
+		    _collisionSize = new Vec2(46f, 10f);
+		    _barrelOffsetTL = new Vec2(46f, 1f);
+            _flare = new SpriteMap(GetPath("FlareOnePixel3"), 13, 10)
             {
                 center = new Vec2(0.0f, 5f)
             };
-            _holdOffset = new Vec2(3f, 2f);
-		    _fireSound = "shotgunFire2";
-		    _kickForce = 4.5f;
-            loseAccuracy = 0.6f;
+            _holdOffset = new Vec2(2f, 2f);
+            _fireSound = GetPath("sounds/shotgun.wav");
+            _kickForce = 9f;
+            loseAccuracy = 0.4f;
             maxAccuracyLost = 0.9f;
-            _fullAuto = true;
-//		    _manualLoad = true;
-            _fireWait = 2.75f;
-            _editorName = "Taligator 6000 SX";
-//          LoaderSprite = new SpriteMap(GetPath("RemingtonPimp"), 6, 8)
-//          {
-//               center = new Vec2(3f, 4f)
-//          };
-//          LoaderVec2 = new Vec2(8f, 0f);
-//          EpsilonA = 50;
-//          EpsilonB = 100;
-//          Loaddx = 2f;
+		    _manualLoad = true;
+            _fireWait = 4f;
+            _editorName = "ATAMAN XP-03";
+            LoaderSprite = new SpriteMap(GetPath("Remington1p"), 16, 4)
+            {
+                    center = new Vec2(8f, 2f)
+            };
+            LoaderVec2 = new Vec2(12f, -1f);
+            Loaddx = 5f;
         }
     }
 }
---
+++ build/src/Taligator6000.cs
@@ -0,0 +1,43 @@
+using DuckGame;
+using JetBrains.Annotations;
+using TMGmod.Core.WClasses;
+
+namespace TMGmod
+{
+    [UsedImplicitly]
+    [EditorGroup("TMG|Shotgun|Full-Auto")]
+    public class Taligator6000 : BaseGun, IAmSg
+    {
+	    public Taligator6000(float xval, float yval) : base(xval, yval)
+	    {
+		    ammo = 11;
+	        _ammoType = new AT9mm
+	        {
+	            range = 120f,
+	            accuracy = 0.47f,
+	            penetration = 1f,
+	            bulletSpeed = 16f,
+	            bulletThickness = 0.6f
+	        };
+            _numBulletsPerFire = 13;
+            _type = "gun";
+		    _graphic = new SpriteMap(GetPath("Taligator 6000 SX"), 31, 12);
+		    _center = new Vec2(16f, 6f);
+		    _collisionOffset = new Vec2(-16f, -6f);
+		    _collisionSize = new Vec2(31f, 12f);
+		    _barrelOffsetTL = new Vec2(31f, 3f);
+            _flare = new SpriteMap(GetPath("FlareOnePixel2"), 13, 10)
+            {
+                center = new Vec2(0.0f, 5f)
+            };
+            _holdOffset = new Vec2(3f, 2f);
+		    _fireSound = "shotgunFire2";
+		    _kickForce = 4.5f;
+            loseAccuracy = 0.6f;
+            maxAccuracyLost = 0.9f;
+            _fullAuto = true;
+            _fireWait = 2.75f;
+            _editorName = "Taligator 6000 SX";
+        }
+    }
+}
