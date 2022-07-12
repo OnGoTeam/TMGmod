28176841d84e6a04348cd433b031e17a9130d00c
timotheyca <tim@ongoteam.yaconnect.com>
2022-07-13 00:28:53 +0300

    Update AssemblyInfo.cs

--- build/Properties/AssemblyInfo.cs
+++ build/Properties/AssemblyInfo.cs
@@ -1,33 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Общие сведения об этой сборке предоставляются следующим набором 
-// атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
-// общие сведения об этой сборке.
 [assembly: AssemblyTitle("TMGmod")]
-[assembly: AssemblyDescription("The Born of HealthBar")]
+[assembly: AssemblyDescription("[1.2] QoL Patches")]
 //[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OnGoTeam")]
 [assembly: AssemblyProduct("TMGmod")]
-[assembly: AssemblyCopyright("Copyright © 2020 OGT")]
+[assembly: AssemblyCopyright("Copyright © 2022 OGT")]
 //[assembly: AssemblyTrademark("")]
 //[assembly: AssemblyCulture("")]
 
-// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
-// для COM-компонентов.  Если необходим доступ к типу в этой сборке из 
-// COM, следует установить атрибут ComVisible в TRUE для этого типа.
 [assembly: ComVisible(false)]
 
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("190a2e48-c24a-4075-89ae-2e60aea4c0f1")]
-
-// Сведения о версии сборки состоят из следующих четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии 
-//   Номер сборки
-//      Редакция
-//
 
 // proposed system:
 // major                            1
