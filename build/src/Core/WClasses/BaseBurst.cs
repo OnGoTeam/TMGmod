5d510731b8c7abb56b8ecd33c12e0ff757551e4e
timotheyca <tim@ongoteam.yaconnect.com>
2022-07-17 23:43:42 +0300

    Update BaseBurst.cs

--- build/src/Core/WClasses/BaseBurst.cs
+++ build/src/Core/WClasses/BaseBurst.cs
@@ -5,12 +5,15 @@ namespace TMGmod.Core.WClasses
 {
     public abstract class BaseBurst : BaseGun
     {
-        protected int BurstNum;
-        protected float DeltaWait;
+        protected int BurstNum = 2;
+        protected float DeltaWait  = .1f;
 
         [UsedImplicitly] public StateBinding ShotsBinding = new StateBinding(nameof(ShotsLeft));
 
         [UsedImplicitly] public int ShotsLeft;
+        [UsedImplicitly] public StateBinding BurstBinding = new StateBinding(nameof(BurstEnabled));
+
+        [UsedImplicitly] public bool BurstEnabled = true;
 
         protected BaseBurst(float xval, float yval) : base(xval, yval)
         {
@@ -20,17 +23,21 @@ namespace TMGmod.Core.WClasses
         {
             if (ShotsLeft > 0 || _wait > 0) return;
             base.Fire();
+            if (!BurstEnabled) return;
+            // else
             ShotsLeft = BurstNum - 1;
             if (ShotsLeft > 0) _wait = DeltaWait;
         }
 
         public override void Update()
         {
+            if (isLocal && !(_wait > 0f) && ShotsLeft > 0)
+            {
+                base.Fire();
+                ShotsLeft -= 1;
+                if (ShotsLeft > 0) _wait = DeltaWait;
+            }
             base.Update();
-            if (!isLocal || _wait > 0f || ShotsLeft <= 0) return;
-            base.Fire();
-            ShotsLeft -= 1;
-            if (ShotsLeft > 0) _wait = DeltaWait;
         }
     }
 }
