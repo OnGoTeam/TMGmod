5ddc42dcee0014ed4d22c6766cc71273d85723af
EXsess <extradan02@gmail.com>
2022-07-17 23:15:40 +0300

    YavaScript

---
+++ build/src/Core/AmmoTypes/ATYava6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DuckGame;
+using TMGmod.Core.Shells;
+
+namespace TMGmod.Core.AmmoTypes
+{
+    // ReSharper disable once InconsistentNaming
+    public class ATYava6 : BaseAmmoType
+    {
+        public ATYava6()
+        {
+            accuracy = 0.77f;
+            penetration = 3f;
+            bulletSpeed = 42f;
+            range = 500f;
+            weight = 5f;
+            bulletThickness = 1.2f;
+            DamageMean = 11f;
+            AlphaDamage = 0.5f;
+            DistanceConvexity = 1;
+        }
+
+        public override void PopShell(float x, float y, int dir, Action<EjectedShell> add)
+        {
+            var shell = new Gauge12Shell(x, y)
+            {
+                hSpeed = (2f + Rando.Float(-0.2f, 0.2f)) * dir,
+                vSpeed = -1.2f + Rando.Float(-0.5f, 0.5f),
+                depth = 0.2f - Rando.Float(0.0f, 0.1f),
+            };
+            add(shell);
+        }
+    }
+}
---
+++ build/src/Yava6.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using DuckGame;
+using System;
+using JetBrains.Annotations;
+using TMGmod.Core;
+using TMGmod.Core.AmmoTypes;
+using TMGmod.Core.WClasses;
+
+namespace TMGmod
+{
+    [EditorGroup("TMG|Rifle|DMR")]
+    // ReSharper disable once InconsistentNaming
+    public class Yava6 : BaseDmr, IHaveSkin
+    {
+        private const int NonSkinFrames = 1;
+        private static readonly List<int> Allowedlst = new List<int>(new[] { 0 });
+        private readonly SpriteMap _sprite;
+        public float DeltaMaxAccuracy;
+
+        [UsedImplicitly]
+        // ReSharper disable once InconsistentNaming
+        private readonly EditorProperty<int> skin;
+
+        public Yava6(float xval, float yval)
+            : base(xval, yval)
+        {
+            skin = new EditorProperty<int>(0, this, -1f, 9f, 0.5f);
+            ammo = 15;
+            _ammoType = new ATYava6();
+            _numBulletsPerFire = 3;
+            MaxAccuracy = _ammoType.accuracy;
+            MinAccuracy = 0.2f;
+            RegenAccuracyDmr = 0.005f;
+            DrainAccuracyDmr = 0.25f;
+            _type = "gun";
+            _sprite = new SpriteMap(GetPath("Yava 6"), 37, 13);
+            _graphic = _sprite;
+            _sprite.frame = 0;
+            _center = new Vec2(19f, 7f);
+            _collisionOffset = new Vec2(-19f, -7f);
+            _collisionSize = new Vec2(37f, 13f);
+            _barrelOffsetTL = new Vec2(37f, 5f);
+            _flare = new SpriteMap(GetPath("FlareOnePixel2"), 13, 10)
+            {
+                center = new Vec2(0.0f, 5f),
+            };
+            _fireSound = GetPath("sounds/scar.wav");
+            _fullAuto = true;
+            _fireWait = 3.2f;
+            _kickForce = 4f;
+            loseAccuracy = 0.1f;
+            maxAccuracyLost = 0.25f;
+            _holdOffset = new Vec2(4f, 1f);
+            ShellOffset = new Vec2(-13f, -4f);
+            _editorName = "Yava 6";
+            _weight = 5f;
+        }
+        public StateBinding FrameIdBinding { get; } = new StateBinding(nameof(FrameId));
+
+        // ReSharper disable once ConvertToAutoProperty
+        public EditorProperty<int> Skin => skin;
+
+        [UsedImplicitly]
+        public int FrameId
+        {
+            get => _sprite.frame;
+            set => _sprite.frame = value % (10 * NonSkinFrames);
+        }
+
+        private void UpdateSkin()
+        {
+            var bublic = Skin.value;
+            while (!Allowedlst.Contains(bublic)) bublic = Rando.Int(0, 9);
+            _sprite.frame = bublic;
+        }
+
+        public override void CalculateAccuracy
+        {
+            DeltaMaxAccuracy += 0.01f;
+            if (Math.Abs(duck.velocity.length) == 0) MaxAccuracy -= DeltaMaxAccuracy;
+            else DeltaMaxAccuracy = 0f;
+        }
+
+        public override void EditorPropertyChanged(object property)
+        {
+            UpdateSkin();
+            base.EditorPropertyChanged(property);
+        }
+    }
+}
