7a979a0bd75b9ea3017c4ee336ed802078d9997e
timotheyca <tim@ongoteam.yaconnect.com>
2022-07-18 17:44:16 +0300

    StockImplementation

---
+++ build/src/Core/StockLogic/StockImplementation.cs
@@ -0,0 +1,60 @@
+﻿using DuckGame;
+
+namespace TMGmod.Core.StockLogic
+{
+    public static class StockImplementation
+    {
+        public static void SetStock<T>(this T target, bool stock) where T : Thing, IHaveStock
+        {
+            var old = target.StockState;
+            if (target.isServerForObject)
+                target.StockState += target.StockSpeed * (stock ? 1 : -1);
+            target.UpdateStockStats(old);
+        }
+
+        public static BitBuffer GetStockBuffer(this IHaveStock target)
+        {
+            var buffer = new BitBuffer();
+            buffer.Write(target.Stock);
+            return buffer;
+        }
+
+        public static void SetStockBuffer(this IHaveStock target, BitBuffer buffer)
+        {
+            target.Stock = buffer.ReadBool();
+        }
+
+        public static void UpdateStock(this IHaveStock target)
+        {
+            var gun = target.AsAGun();
+            if (gun.SwitchStockQ() && (target.Stock || gun.duck.grounded) && gun.duck.inputProfile.Pressed("QUACK"))
+            {
+                target.Stock = !target.Stock;
+
+                SFX.Play("quack", -1, gun.duck.quackPitch);
+            }
+            else if (gun.duck != null)
+            {
+                target.Stock = target.Stock;
+            }
+        }
+
+        public static bool StockDeployed(this IHaveStock target) => target.StockState > .99f;
+        public static bool StockFolded(this IHaveStock target) => target.StockState < .01f;
+
+        public static void UpdateStockSounds(this IHaveStock target, float old)
+        {
+            var gun = target.AsAGun();
+            if (gun.isServerForObject && target.StockDeployed() && old <= 0.99f)
+                SFX.Play(target.StockOn);
+            if (gun.isServerForObject && target.StockFolded() && old >= 0.01f)
+                SFX.Play(target.StockOff);
+        }
+
+        private static bool SwitchStockQ(this Holdable gun)
+        {
+            var duck = gun.duck;
+            return !(duck is null) && !duck.sliding;
+        }
+    }
+}
--- build/src/Core/WClasses/BaseGunImplementations.cs
+++ build/src/Core/WClasses/BaseGunImplementations.cs
@@ -1,22 +1,11 @@
 ﻿using System;
 using System.Linq;
 using DuckGame;
-using JetBrains.Annotations;
-using TMGmod.Core.StockLogic;
 
 namespace TMGmod.Core.WClasses
 {
     public static class BaseGunImplementations
     {
-
-        public static void SetStock<T>(this T target, bool stock) where T : Thing, IHaveStock
-        {
-            var old = target.StockState;
-            if (target.isServerForObject)
-                target.StockState += target.StockSpeed * (stock ? 1 : -1);
-            target.UpdateStockStats(old);
-        }
-
         private static int FilterSkin(this IHaveAllowedSkins target, int skin) =>
             target.AllowedSkins.Contains(skin)
                 ? skin
@@ -24,56 +13,11 @@ namespace TMGmod.Core.WClasses
 
         public static void UpdateSkin(this IHaveAllowedSkins target) =>
             target.FrameId = target.FilterSkin(target.Skin.value);
-        public static BitBuffer GetStockBuffer(this IHaveStock target)
-        {
-            var buffer = new BitBuffer();
-            buffer.Write(target.Stock);
-            return buffer;
-        }
-
-        public static void SetStockBuffer(this IHaveStock target, BitBuffer buffer)
-        {
-            target.Stock = buffer.ReadBool();
-        }
-
-        public static void UpdateStock(this IHaveStock target)
-        {
-            var gun = target.AsAGun();
-            if (gun.SwitchStockQ() && (target.Stock || gun.duck.grounded) && gun.duck.inputProfile.Pressed("QUACK"))
-            {
-                target.Stock = !target.Stock;
-
-                SFX.Play("quack", -1, gun.duck.quackPitch);
-            }
-            else if (gun.duck != null)
-            {
-                target.Stock = target.Stock;
-            }
-        }
-
-        public static bool StockDeployed(this IHaveStock target) => target.StockState > .99f;
-        public static bool StockFolded(this IHaveStock target) => target.StockState < .01f;
-
-        public static void UpdateStockSounds(this IHaveStock target, float old)
-        {
-            var gun = target.AsAGun();
-            if (gun.isServerForObject && target.StockDeployed() && old <= 0.99f)
-                SFX.Play(target.StockOn);
-            if (gun.isServerForObject && target.StockFolded() && old >= 0.01f)
-                SFX.Play(target.StockOff);
-        }
 
         public static bool HandleQ(this Gun gun)
         {
             var duck = gun.duck;
             return !(duck is null) && !gun.raised && duck.sliding && duck.grounded && Math.Abs(duck.hSpeed) < 1f;
-        }
-
-        [UsedImplicitly]
-        public static bool SwitchStockQ(this Gun gun)
-        {
-            var duck = gun.duck;
-            return !(duck is null) && !duck.sliding;
         }
     }
 }
