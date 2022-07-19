0c75037366df0f7fdab9654b6bd4845d44feba50
timotheyca <tim@ongoteam.yaconnect.com>
2022-07-20 01:44:55 +0300

    custom hp

--- build/src/Buddies/HpGiver.cs
+++ build/src/Buddies/HpGiver.cs
@@ -11,8 +11,12 @@ namespace TMGmod.Buddies
     {
         private readonly HashSet<Duck> _given = new HashSet<Duck>();
 
+        [UsedImplicitly]
+        public EditorProperty<float> Hp;
+
         public HpGiver()
         {
+            Hp = new EditorProperty<float>(99f, this, 49f, 9999f, 10f);
             _graphic = new Sprite("swirl");
             _center = new Vec2(8f, 8f);
             _collisionSize = new Vec2(16f, 16f);
@@ -30,7 +34,7 @@ namespace TMGmod.Buddies
                 if (!(thing is Duck duck)) continue;
                 if (!duck.isServerForObject) continue;
                 if (_given.Contains(duck)) continue;
-                var hp = new HpArmor(duck.x, duck.y);
+                var hp = new HpArmor(duck.x, duck.y, Hp.value);
                 Level.Add(hp);
                 // duck.Equip(hp, default, true);
                 duck.Equip(hp);
