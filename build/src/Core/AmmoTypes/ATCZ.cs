d2d32f9670fc6586fd38674455bb17f3bb7c6807
timotheyca <tim@ongoteam.yaconnect.com>
2022-07-21 23:20:12 +0300

    ATCZ

--- build/src/Core/AmmoTypes/ATCZ.cs
+++ build/src/Core/AmmoTypes/ATCZ.cs
@@ -33,30 +33,11 @@ namespace TMGmod.Core.AmmoTypes
     }
 
     // ReSharper disable once InconsistentNaming
-    public class ATCZ2 : BaseAmmoType
+    public class ATCZ2 : ATCZ
     {
         public ATCZ2()
         {
             range = 310f;
-            accuracy = 0.87f;
-            penetration = 1f;
-            bulletSpeed = 40f;
-            bulletThickness = 1f;
-            bulletLength = 50f;
-            DamageMean = 27f;
-            DamageVariation = 0.15f;
-            AlphaDamage = 0.4f;
-            DistanceConvexity = 2f;
-        }
-
-        public override void PopShell(float x, float y, int dir, Action<EjectedShell> add)
-        {
-            var shell = new AT556NATOShell(x, y)
-            {
-                hSpeed = (2.5f + Rando.Float(-0.2f, 0.2f)) * dir,
-                vSpeed = 2f + Rando.Float(-0.3f, 0.3f),
-            };
-            add(shell);
         }
     }
 }
--- build/src/Core/AmmoTypes/ATCZS.cs
+++ build/src/Core/AmmoTypes/ATCZS.cs
@@ -1,7 +1,3 @@
-using System;
-using DuckGame;
-using TMGmod.Core.Shells;
-
 namespace TMGmod.Core.AmmoTypes
 {
     // ReSharper disable once InconsistentNaming
@@ -13,50 +9,19 @@ namespace TMGmod.Core.AmmoTypes
             accuracy = 0.95f;
             penetration = 0.4f;
             bulletSpeed = 37f;
-            bulletThickness = 1f;
-            bulletLength = 0f;
+            bulletThickness = 0f;
             DamageMean = 26f;
             DamageVariation = 0.1f;
             AlphaDamage = 0.37f;
-            DistanceConvexity = 2f;
-        }
-
-        public override void PopShell(float x, float y, int dir, Action<EjectedShell> add)
-        {
-            var shell = new AT556NATOShell(x, y)
-            {
-                hSpeed = (2.5f + Rando.Float(-0.2f, 0.2f)) * dir,
-                vSpeed = 2f + Rando.Float(-0.3f, 0.3f),
-            };
-            add(shell);
         }
     }
 
     // ReSharper disable once InconsistentNaming
-    public class ATCZS2 : BaseAmmoType
+    public class ATCZS2 : ATCZS
     {
         public ATCZS2()
         {
             range = 350f;
-            accuracy = 0.95f;
-            penetration = 0.4f;
-            bulletSpeed = 37f;
-            bulletThickness = 1f;
-            bulletLength = 0f;
-            DamageMean = 25f;
-            DamageVariation = 0.1f;
-            AlphaDamage = 0.37f;
-            DistanceConvexity = 2f;
-        }
-
-        public override void PopShell(float x, float y, int dir, Action<EjectedShell> add)
-        {
-            var shell = new AT556NATOShell(x, y)
-            {
-                hSpeed = (2.5f + Rando.Float(-0.2f, 0.2f)) * dir,
-                vSpeed = 2f + Rando.Float(-0.3f, 0.3f),
-            };
-            add(shell);
         }
     }
 }
