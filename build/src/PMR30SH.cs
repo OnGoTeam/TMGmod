9ee97640c4e4fbb91ff0b02664e52a4b537077a4
timotheyca <tim@ongoteam.yaconnect.com>
2022-07-22 17:21:31 +0300

    Update PMR30SH.cs

--- build/src/PMR30SH.cs
+++ build/src/PMR30SH.cs
@@ -2,6 +2,7 @@
 using DuckGame;
 using JetBrains.Annotations;
 using TMGmod.Core.AmmoTypes;
+using TMGmod.Core.Modifiers.Updating;
 using TMGmod.Core.SkinLogic;
 using TMGmod.Core.WClasses;
 using TMGmod.Core.WClasses.ClassMarkers;
@@ -9,6 +10,7 @@ using TMGmod.Core.WClasses.ClassMarkers;
 namespace TMGmod
 {
     [EditorGroup("TMG|Handgun|Custom")]
+    [UsedImplicitly]
     // ReSharper disable once InconsistentNaming
     public class PMR : BaseGun, IAmHg, IHaveAllowedSkins
     {
@@ -34,21 +36,13 @@ namespace TMGmod
         private readonly float[] _loseAccuracym = { .1f, 0f };
         private readonly float[] _maxAccuracyLostm = { .55f, 0f };
         private readonly int[] _numBulletsPerFirem = { 1, 16 };
-        private bool _switched;
-        [UsedImplicitly] public int Ammom0 = 30;
-        [UsedImplicitly] public StateBinding Ammom0Binding = new StateBinding(nameof(Ammom0));
-        [UsedImplicitly] public int Ammom1 = 1;
-        [UsedImplicitly] public StateBinding Ammom1Binding = new StateBinding(nameof(Ammom1));
-        [UsedImplicitly] public int Mode;
-        [UsedImplicitly] public StateBinding ModeBinding = new StateBinding(nameof(Mode));
 
         public PMR(float xval, float yval)
             : base(xval, yval)
         {
-            ammo = 30;
             _ammoType = _ammoTypem[0];
             _numBulletsPerFire = 1;
-            
+
             _sprite = new SpriteMap(GetPath("PMR30Custom"), 16, 10);
             _graphic = _sprite;
             _center = new Vec2(8f, 5f);
@@ -66,59 +60,25 @@ namespace TMGmod
             maxAccuracyLost = 0.5f;
             _editorName = "PMR30 Shotgunned";
             _weight = 1f;
-            FrameId = FrameId % 10 + 10 * (_switched ? 1 + Mode : 0);
+            Compose(
+                new SwitchingModes(
+                    this,
+                    new[] { 30, 1 },
+                    mode =>
+                    {
+                        FrameId = FrameId % 10 + 10 * (1 + mode);
+                        _ammoType = _ammoTypem[mode];
+                        _barrelOffsetTL = _barrelOffsetTLm[mode];
+                        _fireSound = _fireSoundm[mode];
+                        loseAccuracy = _loseAccuracym[mode];
+                        maxAccuracyLost = _maxAccuracyLostm[mode];
+                        _numBulletsPerFire = _numBulletsPerFirem[mode];
+                    },
+                    () => FrameId %= 10
+                )
+            );
         }
 
-        [UsedImplicitly]
-        public int[] Ammom
-        {
-            get => new[] { Ammom0, Ammom1 };
-            set
-            {
-                Ammom0 = value[0];
-                Ammom1 = value[1];
-            }
-        }
-
-        private void UpdateMode()
-        {
-            FrameId = FrameId % 10 + 10 * (_switched ? 1 + Mode : 0);
-            _ammoType = _ammoTypem[Mode];
-            _barrelOffsetTL = _barrelOffsetTLm[Mode];
-            _fireSound = _fireSoundm[Mode];
-            loseAccuracy = _loseAccuracym[Mode];
-            maxAccuracyLost = _maxAccuracyLostm[Mode];
-            _numBulletsPerFire = _numBulletsPerFirem[Mode];
-        }
-
-        public override void Update()
-        {
-            if (duck is null)
-            {
-                Mode = 0;
-                _switched = false;
-            }
-            else if (duck.inputProfile.Pressed("QUACK"))
-            {
-                Mode = 1 - Mode;
-                _switched = true;
-                SFX.Play(GetPath("sounds/tuduc.wav"));
-            }
-
-            UpdateMode();
-            base.Update();
-        }
-
-        public override void Fire()
-        {
-            ammo = Ammom[Mode];
-            base.Fire();
-            if (Mode == 0)
-                Ammom0 = ammo;
-            else
-                Ammom1 = ammo;
-            ammo = Ammom[0] + Ammom[1];
-        }
         public ICollection<int> AllowedSkins { get; } = new List<int>(new[] { 0 });
 
         public StateBinding FrameIdBinding { get; } = new StateBinding(nameof(FrameId));
