c285cac7f1c5b9e3f0aab7d24710b74e37c19465
timotheyca <tim@ongoteam.yaconnect.com>
2022-07-23 20:56:32 +0300

    Update Mod.cs

--- build/src/Core/Mod.cs
+++ build/src/Core/Mod.cs
@@ -1,5 +1,10 @@
 ﻿using DuckGame;
 using JetBrains.Annotations;
+#if DEBUG
+using System.Linq;
+using System.IO;
+using TMGmod.Core.WClasses;
+#endif
 
 namespace TMGmod.Core
 {
@@ -17,6 +22,35 @@ namespace TMGmod.Core
         protected override void OnPostInitialize()
         {
             MapPack.LoadMapPack(GetPath<TMGmod>("TMG Levels"));
+#if DEBUG
+            var placeables = ItemBox.GetPhysicsObjects(Editor.Placeables);
+            placeables.RemoveAll(t => !t.IsSubclassOf(typeof(BaseGun)));
+            var guns = placeables.Select(
+                Editor.CreateThing
+            ).Select(
+                gun => gun as BaseGun
+            ).Where(
+                based => based != null
+            );
+            var lines = guns.Select(
+                gun => gun.StatsLine().Select(s => s.Replace(".", ","))
+            );
+            lines = new[] { BaseGun.StatsHeader() }.Concat(lines);
+            var joined = lines.Select(
+                e => string.Join(";", e)
+            );
+            var text = string.Join(
+                "\n",
+                joined
+            );
+            Debug.Log($"BaseGun Count: {placeables.Count}");
+            var stats = GetPath<TMGmod>("stats");
+            Directory.CreateDirectory(stats);
+            File.WriteAllText(
+                Path.Combine(stats, "guns.csv"),
+                text
+            );
+#endif
         }
     }
 }
