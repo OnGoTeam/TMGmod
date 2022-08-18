3daf7c5fd9146e64f975b50abfb060e6647d89c4
timotheyca <tim@ongoteam.yaconnect.com>
2022-08-18 10:19:35 +0300

    SynchronizedValue

---
+++ build/src/Core/Modifiers/Updating/SynchronizedValue.cs
@@ -0,0 +1,24 @@
+﻿using DuckGame;
+
+namespace TMGmod.Core.Modifiers.Updating
+{
+    public class SynchronizedValue<T> : Modifier
+    {
+        public T Value;
+
+        public SynchronizedValue(T value)
+        {
+            Value = value;
+        }
+
+        protected override void Read(BitBuffer buffer)
+        {
+            Value = buffer.Read<T>();
+        }
+
+        protected override void Write(BitBuffer buffer)
+        {
+            buffer.Write(Value);
+        }
+    }
+}
