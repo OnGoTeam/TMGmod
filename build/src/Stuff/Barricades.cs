5129277f76d9f3424e241d57680cbec18c75710c
timotheyca <tim@ongoteam.yaconnect.com>
2022-08-20 13:01:58 +0300

    Update Barricades.cs

--- build/src/Stuff/Barricades.cs
+++ build/src/Stuff/Barricades.cs
@@ -3,16 +3,27 @@ using JetBrains.Annotations;
 
 namespace TMGmod.Stuff
 {
-    //[EditorGroup("TMG|Misc")]
+    [EditorGroup("TMG|Misc")]
     [BaggedProperty("CanSpawn", false)]
     [UsedImplicitly]
     public class Barricades : GreyBlock
     {
         private bool _deployed;
 
+        [UsedImplicitly] public EditorProperty<int> Height;
+
         public Barricades(float xpos, float ypos) : base(xpos, ypos)
         {
+            Height = new EditorProperty<int>(2, this, 1, 4);
             _editorName = "Barricades";
+        }
+
+        public override void EditorPropertyChanged(object property)
+        {
+            if (Height.value > 4)
+                Height.value = 4;
+            if (Height.value < 1)
+                Height.value = 1;
         }
 
         public override void Update()
@@ -26,17 +37,18 @@ namespace TMGmod.Stuff
         {
             if (!isServerForObject) return;
             if (Level.activeLevel is Editor) return;
-            for (var i = 0; i < 8; ++i)
-            {
-                var barricade = new Barricade(vec2.x, vec2.y - i * 4);
-                Level.Add(barricade);
-            }
+            for (var i = 0; i < Height.value * 4; ++i)
+                Level.Add(new Barricade(vec2.x, vec2.y - i * 4));
         }
 
         public override void Draw()
         {
             if (Level.activeLevel is Editor)
-                Graphics.DrawRect(position + new Vec2(1, -8), position + new Vec2(-1, -40), new Color(63, 127, 0, 127));
+                Graphics.DrawRect(
+                    position + new Vec2(1, -8),
+                    position + new Vec2(-1, -8) + new Vec2(0, -4) * Height.value * 4,
+                    new Color(63, 127, 0, 127)
+                );
             base.Draw();
         }
     }
