099dd970e19876ee5ea495e609c38dd8bfcf2ec7
timotheyca <tim@ongoteam.yaconnect.com>
2022-08-22 17:44:42 +0300

    Update FrameUtils.cs

--- build/src/Core/FrameUtils.cs
+++ build/src/Core/FrameUtils.cs
@@ -2,9 +2,6 @@
 {
     public static class FrameUtils
     {
-        public static int Modulo(this int value, int mod)
-        {
-            return (value % mod + mod) % mod;
-        }
+        public static int Modulo(this int value, int mod) => (value % mod + mod) % mod;
     }
 }
