5601844f0545173ea22e3baeab3ac368136be095
timotheyca <tim@ongoteam.yaconnect.com>
2022-08-22 17:44:25 +0300

    FrameUtils.Modulo

---
+++ build/src/Core/FrameUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace TMGmod.Core
+{
+    public static class FrameUtils
+    {
+        public static int Modulo(this int value, int mod)
+        {
+            return (value % mod + mod) % mod;
+        }
+    }
+}
