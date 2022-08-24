ed6ecb5c4055b2682260a8469fd6bb33e25027e1
timotheyca <tim@ongoteam.yaconnect.com>
2022-08-24 14:35:14 +0300

    Update SIX12S.cs

--- build/src/SIX12S.cs
+++ build/src/SIX12S.cs
@@ -3,6 +3,7 @@ using DuckGame;
 using JetBrains.Annotations;
 using TMGmod.AmmoTypes;
 using TMGmod.Core;
+using TMGmod.Core.Modifiers.Updating;
 using TMGmod.Core.SkinLogic;
 using TMGmod.Core.WClasses;
 using TMGmod.Core.WClasses.ClassMarkers;
@@ -13,8 +14,6 @@ namespace TMGmod
     // ReSharper disable once InconsistentNaming
     public class SIX12S : BaseGun, IHaveAllowedSkins, IAmSg, I5
     {
-        [UsedImplicitly] public StateBinding LaserBinding = new StateBinding(nameof(laserSight));
-
         public SIX12S(float xval, float yval)
             : base(xval, yval)
         {
@@ -39,39 +38,25 @@ namespace TMGmod
             _laserOffsetTL = new Vec2(24f, 7.5f);
             _holdOffset = new Vec2(1f, 0f);
             _weight = 4f;
+            Compose(new Quacking(this, true, true, () => LaserSight = !LaserSight));
         }
 
         public ICollection<int> AllowedSkins { get; } = new List<int>(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 9 });
 
-        public override void Update()
+        public bool LaserSight
         {
-            if (Quacked())
+            get => laserSight;
+            set
             {
-                if (laserSight)
-                {
-                    NonSkin = 0;
-                    loseAccuracy = 0.3f;
-                    maxAccuracyLost = 0.5f;
-                    laserSight = false;
-                }
-                else
-                {
-                    NonSkin = 1;
-                    loseAccuracy = 0.45f;
-                    maxAccuracyLost = 0.5f;
-                    laserSight = true;
-                }
-
-                SFX.Play(GetPath("sounds/tuduc.wav"));
+                if (value != LaserSight)
+                    SFX.Play(GetPath("sounds/tuduc.wav"));
+                laserSight = value;
+                NonSkin = value ? 1 : 0;
+                loseAccuracy = value ? .45f : .3f;
             }
-
-            base.Update();
         }
+        [UsedImplicitly] public StateBinding LaserBinding = new StateBinding(nameof(LaserSight));
 
-        public override void Reload(bool shell = true)
-        {
-            if (ammo != 0) --ammo;
-            loaded = true;
-        }
+        public override void Reload(bool shell = true) => base.Reload(false);
     }
 }
