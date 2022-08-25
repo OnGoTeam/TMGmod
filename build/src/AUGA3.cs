9dfceb09a57a3e938f91d97b0f4b96c6a559f89b
timotheyca <tim@ongoteam.yaconnect.com>
2022-08-25 16:53:01 +0300

    Update AUGA3.cs

--- build/src/AUGA3.cs
+++ build/src/AUGA3.cs
@@ -3,7 +3,6 @@ using DuckGame;
 using JetBrains.Annotations;
 using TMGmod.AmmoTypes;
 using TMGmod.Core;
-using TMGmod.Core.BipodsLogic;
 using TMGmod.Core.SkinLogic;
 using TMGmod.Core.WClasses.ClassImplementations;
 
@@ -11,16 +10,17 @@ namespace TMGmod
 {
     [EditorGroup("TMG|Rifle|Fully-Automatic")]
     // ReSharper disable once InconsistentNaming
-    public class AUGA3 : BaseAr, IHaveAllowedSkins, IHaveBipods
+    public class AUGA3 : BaseAr, IHaveAllowedSkins
     {
         [UsedImplicitly] public StateBinding HandAngleOffBinding = new StateBinding(nameof(HandAngleOff));
+
         public AUGA3(float xval, float yval)
             : base(xval, yval)
         {
             _editorName = "AUG A3";
             ammo = 30;
             SetAmmoType<ATAUGA3>();
-            
+
             Smap = new SpriteMap(GetPath("AUGA3"), 30, 12);
             _center = new Vec2(15f, 6f);
             _collisionOffset = new Vec2(-15f, -6f);
@@ -40,25 +40,19 @@ namespace TMGmod
             _laserOffsetTL = new Vec2(15f, 0.5f);
             KforceDelta = .63f;
         }
+
         public float HandAngleOff
         {
             get => handAngle * offDir;
             set => handAngle = value * offDir;
         }
 
-        public BitBuffer BipodsBuffer
-        {
-            get => this.GetBipodBuffer();
-            set => this.SetBipodBuffer(value);
-        }
-        public bool Bipods
-        {
-            get => BipodsQ();
-            set => laserSight = value;
-        }
-        public StateBinding BipodsBinding { get; } = new StateBinding(nameof(BipodsBuffer));
-        public bool BipodsDisabled => false;
-
         public ICollection<int> AllowedSkins { get; } = new List<int>(new[] { 0, 1, 8 });
+
+        public override void Update()
+        {
+            laserSight = BipodsQ();
+            base.Update();
+        }
     }
 }
