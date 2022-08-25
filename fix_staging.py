10772ded5bda0abe9e85dda539db52f73bb61709
timotheyca <tim@ongoteam.yaconnect.com>
2022-08-25 17:10:30 +0300

    Update fix_staging.py

--- fix_staging.py
+++ fix_staging.py
@@ -3,19 +3,22 @@ from pathlib import Path
 from uuid import uuid4
 
 
-wid = {
-    'prod': b'workshopIDs\r\x00\x00\x00\x01\x00\x00\x00\x01\x13\x03\xc1\x42',
-    'stgn': b'workshopIDs\r\x00\x00\x00\x01\x00\x00\x00\x01\x9b\xe9\x19\xaa',
-}[input()]
+mode = input()
+did = b'workshopIDs\r\x00\x00\x00\x01\x00\x00\x00\x01\x91\x03\x89\x72'
+pid = b'workshopIDs\r\x00\x00\x00\x01\x00\x00\x00\x01\x13\x03\xc1\x42'
+sid = b'workshopIDs\r\x00\x00\x00\x01\x00\x00\x00\x01\x9b\xe9\x19\xaa'
+if mode != 'dev':
+    wid = { 'prod': pid, 'stgn': sid }[mode]
 
 
 def fix(fn: str):
     with open(fn, 'rb') as f:
         b = f.read()
-    b = b.replace(
-        b'workshopIDs\r\x00\x00\x00\x01\x00\x00\x00\x01\x91\x03\x89\x72',
-        wid
-    )
+    if mode == 'dev':
+        b = b.replace(pid, did)
+        b = b.replace(sid, did)
+    else:
+        b = b.replace(did, wid)
     with open(fn, 'wb') as f:
         f.write(b)
 
