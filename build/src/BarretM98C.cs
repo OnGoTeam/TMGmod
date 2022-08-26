dc6e2336776504cd1e475c8575a035c190488cdb
timotheyca <tim@ongoteam.yaconnect.com>
2022-08-27 01:04:48 +0300

    Update BarretM98C.cs

--- build/src/BarretM98C.cs
+++ build/src/BarretM98C.cs
@@ -39,17 +39,11 @@ namespace TMGmod
             base.OnInitialize();
         }
 
-        protected override bool HasLaser()
-        {
-            return false;
-        }
+        protected override bool HasLaser() => false;
 
         protected override float MaxAngle() => 0.1f;
 
-        protected override float MaxOffset()
-        {
-            return 4.0f;
-        }
+        protected override float MaxOffset() => 4.0f;
 
         protected override float ReloadSpeed() => .5f;
     }
