5f607da3530bb3c7d98ca66aa59ca51a88847406
timotheyca <tim@ongoteam.yaconnect.com>
2022-08-27 16:36:40 +0300

    new map fix

--- fix_staging.py
+++ fix_staging.py
@@ -1,24 +1,22 @@
 import os
+import sys
 from pathlib import Path
 from uuid import uuid4
 
 
-mode = input()
+mode = input().strip()
 did = b'workshopIDs\r\x00\x00\x00\x01\x00\x00\x00\x01\x91\x03\x89\x72'
 pid = b'workshopIDs\r\x00\x00\x00\x01\x00\x00\x00\x01\x13\x03\xc1\x42'
 sid = b'workshopIDs\r\x00\x00\x00\x01\x00\x00\x00\x01\x9b\xe9\x19\xaa'
-if mode != 'dev':
-    wid = { 'prod': pid, 'stgn': sid }[mode]
+ids = { 'Workshop': pid, 'Staging': sid, 'Dev': did }
+wid = ids[mode]
 
 
 def fix(fn: str):
     with open(fn, 'rb') as f:
         b = f.read()
-    if mode == 'dev':
-        b = b.replace(pid, did)
-        b = b.replace(sid, did)
-    else:
-        b = b.replace(did, wid)
+    for qid in ids.values():
+        b = b.replace(qid, wid)
     with open(fn, 'wb') as f:
         f.write(b)
 
@@ -26,4 +24,3 @@ def fix(fn: str):
 for path in Path('content/TMG Levels').glob('*.lev'):
     fix(path)
 print('done')
-input()
